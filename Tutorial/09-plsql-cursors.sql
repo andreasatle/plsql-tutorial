a24ed1547c4c59f6ebdf3af8a201ec42005516b2
Andreas Atle <atle.andreas@gmail.com>
2024-01-05 19:25:54 -0600

    Update the cursor script.

--- Tutorial/09-plsql-cursors.sql
+++ Tutorial/09-plsql-cursors.sql
@@ -1,6 +1,8 @@
 /*
 Explicit cursors are used to process the rows returned by a query.
 The cursor is declared, opened, fetched and closed.
+
+Implicit cursors OPEN, FETCH and CLOSE the cursor automatically.
 */
 SET SERVEROUTPUT ON;
 
@@ -16,6 +18,11 @@ BEGIN
     LOOP
         FETCH c_customers INTO l_customer;
         EXIT WHEN c_customers%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE(l_customer.name);
+    END LOOP;
+    CLOSE c_customers;
+
+    FOR l_customer IN c_customers LOOP
         DBMS_OUTPUT.PUT_LINE(l_customer.name);
     END LOOP;
 END;
